633c05de7fb877092dfbcf02192e130a090c5b2f
Sulabh Katila <sulabhkatila@gmail.com>
2026-06-06 09:32:08 -0400

    I'M BATMANNNN

---
+++ MBatmaN/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MBatmaN
+//
+// Created by Sulabh Katila on 6/6/26
+//  Main interface for the BatMapper iOS app.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("I'M BATMANNN!")
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MBatmaN/MBatmaNApp.swift
@@ -0,0 +1,18 @@
+//
+//  MBatmaNApp.swift
+//  MBatmaN
+//
+//  Created by Sulabh Katila on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MBatmaNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
